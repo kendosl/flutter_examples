ab1d3031ac28dc6872ce0b032fb4b7ac9bb6c879
kendosl <116136261+kendosl@users.noreply.github.com>
2022-12-23 09:24:23 +0700

    Add files via upload

--- mvvm-with-provider-example-1/service/api.dart
+++ mvvm-with-provider-example-1/service/api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:html';
 
+import 'package:flutter/foundation.dart';
 import 'package:mvvm_with_provider_example_1/models/post.dart';
 import 'package:mvvm_with_provider_example_1/notifiers/posts_notifier.dart';
 import 'package:http/http.dart' as http;
@@ -11,16 +12,17 @@ class ApiService {
 
   static getPosts(PostsNotifier postsNotifier) async {
     List<Post> postList = [];
-    http.get(Uri(query: API_ENDPOINT)).then((response) {
-      print('Response status: ${response.statusCode}');
+    http.get(Uri.parse(API_ENDPOINT)).then((response) {
       if (response.statusCode == 200) {
-        print('Response body: ${response.body}');
+        if (kDebugMode) {
+          print('Response body: ${response.body}');
+        }
       }
 
       List posts = jsonDecode(response.body);
-      posts.forEach((element) {
+      for (var element in posts) {
         postList.add(Post.fromMap(element));
-      });
+      }
 
       postsNotifier.setPostList(postList);
     });
@@ -29,7 +31,7 @@ class ApiService {
   static Future<bool> addPost(Post post, PostsNotifier postsNotifier) async {
     bool result = false;
     await http
-        .post(Uri(query: API_ENDPOINT),
+        .post(Uri.parse(API_ENDPOINT),
             headers: {"Content-type": "application/json; charset=UTF-8"},
             body: json.encode(post.toMap()))
         .then((response) {
