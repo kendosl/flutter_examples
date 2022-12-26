36a248a87690dcdce529b16c9988575402c32874
kendosl <116136261+kendosl@users.noreply.github.com>
2022-12-26 13:44:30 +0700

    Add files via upload

---
+++ mvvm_with_provider_example_3/shared/components/notifier/notifier.component.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class NotifierWidget<T extends ChangeNotifier> extends StatefulWidget {
+  const NotifierWidget({
+    Key? key,
+    required this.builder,
+    required this.model,
+    this.child,
+  }) : super(key: key);
+
+  final T model;
+  final Widget? child;
+  final Widget Function(BuildContext context, T model, Widget? child) builder;
+
+  @override
+  State<NotifierWidget> createState() => _NotifierWidgetState();
+}
+
+class _NotifierWidgetState<T extends ChangeNotifier>
+    extends State<NotifierWidget<T>> {
+  late T model;
+
+  @override
+  void initState() {
+    model = widget.model;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => model,
+      child: Consumer<T>(
+        builder: widget.builder,
+        child: widget.child,
+      ),
+    );
+  }
+}
---
+++ mvvm_with_provider_example_3/shared/routes/routes.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class NamedRoute {
+  NamedRoute._();
+
+  static const String home = '/';
+  static const String pellen = '/pellen';
+  static const String fringilla = '/fringilla';
+}
+
+class AppRoutes {
+  AppRoutes._();
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case NamedRoute.home:
+        return MaterialPageRoute(
+          builder: (context) => const HomeView(),
+          settings: settings,
+        );
+      case NamedRoute.pellen:
+        return MaterialPageRoute(
+          builder: (context) => const PellenView(),
+          settings: settings,
+        );
+      case NamedRoute.fringilla:
+        return MaterialPageRoute(
+          builder: (context) => const FringillaView(),
+          settings: settings,
+        );
+      default:
+        return MaterialPageRoute(
+            builder: (context) => _UndefinedView(name: settings.name),
+            settings: settings);
+    }
+  }
+}
+
+class _UndefinedView extends StatelessWidget {
+  const _UndefinedView({Key? key, this.name}) : super(key: key);
+  final String? name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Something went wrong for $name'),
+      ),
+    );
+  }
+}
